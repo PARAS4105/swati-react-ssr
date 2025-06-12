2109d00ae902a073391e379575a4f607feec821f
raj1872 <raj.t@coronation.in>
2025-06-12 21:22:00 +0530

    Uploading Server.js

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ if (url === "/") {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>"SSR App"</title>
+      <title>SSR App</title>
 
       <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
